f8d6f19913e2cb1cf1ebdff0192ea6a28f574578
PhanVanManh <imseirousmanh@gmail.com>
2026-01-24 17:27:34 +0700

    Write implementation for UserDetailServiceImpl

---
+++ src/main/java/com/quiz/quizproject/service/auth/impl/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.quiz.quizproject.service.auth.impl;
+
+import com.quiz.quizproject.entity.UserEntity;
+import com.quiz.quizproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return      User
+                .withUsername(user.getUserName())
+                .password(user.getPassword())
+                .roles(user.getRoles().stream().map(r -> r.getName().replace("ROLE_", "")).toArray(String[]::new))
+                .build();
+    }
+}
