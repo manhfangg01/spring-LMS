5fdddaea4a0fbdfdc0f2af57926c2d353b300785
PhanVanManh <imseirousmanh@gmail.com>
2026-01-24 17:27:14 +0700

    Write implementation for JwtService

--- src/main/java/com/quiz/quizproject/service/auth/JwtService.java
+++ src/main/java/com/quiz/quizproject/service/auth/JwtService.java
@@ -1,101 +1,22 @@
 package com.quiz.quizproject.service.auth;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
-import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 @Service
-public class JwtService {
+public interface JwtService {
+    String generateAccessToken(UserDetails userDetails);
 
-    @Value("${SECRET-KEY}")
-    private String secret;
+    String generateRefreshToken(UserDetails userDetails);
 
-    @Value("${ACCESS_VALID_TIME}")
-    private long accessValidTime;
+    String extractUsername(String token);
 
-    @Value("${REFRESH_VALID_TIME}")
-    private long refreshValidTime;
+    boolean isTokenValid(String token, UserDetails userDetails);
 
-    public SecretKey getSignInKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
 
-
-    private String buildToken(
-            Map<String, Object> extraClaims,
-            UserDetails userDetails,
-            long expiration
-    ) {
-        return Jwts.builder()
-                .claims(extraClaims)
-                .subject(userDetails.getUsername())
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSignInKey()) // Key bí mật của bạn
-                .compact();
-    }
-
-    public String generateAccessToken(UserDetails userDetails) {
-        return buildToken(new HashMap<>(), userDetails, accessValidTime);
-    }
-
-    public String generateRefreshToken(UserDetails userDetails) {
-        return buildToken(new HashMap<>(), userDetails, refreshValidTime);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .verifyWith(getSignInKey()) // Dùng Key để xác thực chữ ký
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-    }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
 
 }
-
-
-/*
-Request đi kèm Header Authorization: Bearer <token>.
-
-Filter gọi extractUsername(token) -> Nếu lấy được "admin", nó bắt đầu tin 50%.
-
-Filter gọi userDetailsService.loadUserByUsername("admin") để lấy thông tin từ DB.
-
-Filter gọi isTokenValid(token, userDetails) -> Nếu trả về true, lúc này nó mới tin 100% và cho phép vào hệ thống.x
- */
---
+++ src/main/java/com/quiz/quizproject/service/auth/impl/JwtServiceImpl.java
@@ -0,0 +1,99 @@
+package com.quiz.quizproject.service.auth.impl;
+
+import com.quiz.quizproject.service.auth.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    @Value("${SECRET-KEY}")
+    private String secret;
+
+    @Value("${ACCESS_VALID_TIME}")
+    private long accessValidTime;
+
+    @Value("${REFRESH_VALID_TIME}")
+    private long refreshValidTime;
+
+    private SecretKey getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration
+    ) {
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey()) // Key bí mật của bạn
+                .compact();
+    }
+
+    public String generateAccessToken(UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, accessValidTime);
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, refreshValidTime);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey()) // Dùng Key để xác thực chữ ký
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+}
+
+
+/*
+Request đi kèm Header Authorization: Bearer <token>.
+
+Filter gọi extractUsername(token) -> Nếu lấy được "admin", nó bắt đầu tin 50%.
+
+Filter gọi userDetailsService.loadUserByUsername("admin") để lấy thông tin từ DB.
+
+Filter gọi isTokenValid(token, userDetails) -> Nếu trả về true, lúc này nó mới tin 100% và cho phép vào hệ thống.x
+ */
