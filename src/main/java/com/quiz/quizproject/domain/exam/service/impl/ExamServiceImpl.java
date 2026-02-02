d0abe13473c3b2a98c16cf99618cc062b6d45793
PhanVanManh <imseirousmanh@gmail.com>
2026-02-02 20:26:36 +0700

    domain exam: push complement implementation of service

---
+++ src/main/java/com/quiz/quizproject/domain/exam/service/impl/ExamServiceImpl.java
@@ -0,0 +1,64 @@
+package com.quiz.quizproject.domain.exam.service.impl;
+
+import com.quiz.quizproject.domain.exam.ExamEntity;
+import com.quiz.quizproject.domain.exam.dto.request.ExamRequest;
+import com.quiz.quizproject.domain.exam.dto.response.ExamResponse;
+import com.quiz.quizproject.domain.exam.filter.ExamFilter;
+import com.quiz.quizproject.domain.exam.mapper.ExamMapper;
+import com.quiz.quizproject.domain.exam.repo.ExamRepository;
+import com.quiz.quizproject.domain.exam.service.ExamService;
+import com.quiz.quizproject.util.exception.handler.AppException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamServiceImpl implements ExamService {
+
+    private final ExamRepository examRepository;
+    private final ExamMapper examMapper;
+
+    @Override
+    public Page<ExamResponse> getExams(ExamFilter filter, Pageable pageable) {
+        return examRepository.findAll(filter.toSpecification(), pageable)
+                .map(examMapper::toResponse);
+    }
+
+    @Override
+    public ExamResponse getExamById(Long id) {
+        ExamEntity exam = examRepository.findById(id)
+                .orElseThrow(() -> new AppException("ApiException", HttpStatus.NOT_FOUND, "Không tìm thấy",
+                        "Bài thi không tồn tại"));
+        return examMapper.toResponse(exam);
+    }
+
+    @Override
+    public ExamResponse createExam(ExamRequest request) {
+        if (examRepository.existsByTitle(request.title())) {
+            throw new AppException("ApiException", HttpStatus.BAD_REQUEST, "Lỗi nhập liệu",
+                    "Tiêu đề bài thi đã tồn tại");
+        }
+        ExamEntity exam = examMapper.toEntity(request);
+        return examMapper.toResponse(examRepository.save(exam));
+    }
+
+    @Override
+    public ExamResponse updateExam(Long id, ExamRequest request) {
+        ExamEntity exam = examRepository.findById(id)
+                .orElseThrow(() -> new AppException("ApiException", HttpStatus.NOT_FOUND, "Không tìm thấy",
+                        "Bài thi không tồn tại"));
+        examMapper.updateEntityFromRequest(request, exam);
+        return examMapper.toResponse(examRepository.save(exam));
+    }
+
+    @Override
+    public void deleteExam(Long id) {
+        if (!examRepository.existsById(id)) {
+            throw new AppException("ApiException", HttpStatus.NOT_FOUND, "Không tìm thấy", "Bài thi không tồn tại");
+        }
+        examRepository.deleteById(id);
+    }
+}
